facd861a23c44ab04115f13893644ae1abe684b0
fforius <60130637+fforius@users.noreply.github.com>
2020-02-24 10:54:29 +0700

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -19,7 +19,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 2
+        lim = 4
     response = googleimagesdownload()
 
     # creating list of arguments
