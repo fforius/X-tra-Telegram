dddcae2974875dea0554420d78b0ed62c1018a7b
fforius <60130637+fforius@users.noreply.github.com>
2020-03-15 03:48:01 +0700

    fix imgg

--- userbot/google_images_download.py
+++ userbot/google_images_download.py
@@ -674,7 +674,7 @@ class googleimagesdownload:
                 download_message = "URLError on an image...trying next one..." + " Error: " + str(e)
                 return_image_name = ''
                 absolute_path = ''
-                
+
             except BadStatusLine as e:
                 download_status = 'fail'
                 download_message = "BadStatusLine on an image...trying next one..." + " Error: " + str(e)
@@ -745,7 +745,7 @@ class googleimagesdownload:
 
     # Getting all links with the help of '_images_get_next_image'
     def _get_image_objects(self,s):
-        start_line = s.find("AF_initDataCallback({key: \\'ds:2\\'") - 10
+        start_line = s.find("AF_initDataCallback({key: \\'ds:1\\'") - 10
         start_object = s.find('[', start_line + 1)
         end_object = s.find('</script>', start_object + 1) - 4
         object_raw = str(s[start_object:end_object])
